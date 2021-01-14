d3e8aba98e8b6180d2621a4ccbd0984ab77dd20b
Nicol-git-gud <capitanvizzi@gmail.com>
2021-01-14 23:16:56 +0100

    BELLA ZI

--- righe.cpp
+++ righe.cpp
@@ -6,17 +6,22 @@ int main()
 {
     std::ifstream f;
     std::string filename = "test.txt";
-    f.open(filename,std::ifstream::in);
-     if (!f.good()) 
-     throw std::string("Non posso aprire il file dati");
-    
-     std::string testo;
+    f.open(filename, std::ifstream::in);
+    if (!f.good())
+        throw std::string("Non posso aprire il file dati");
+
+    std::string testo;
     int line = 1;
-    while (getline(f,testo)) { 
-     
-        std::cout  <<line << " : " << testo << std::endl;
-        line++;
+    char c = f.get();
+    std::cout << line << ": ";
+    while (f.good())
+    {
+        std::cout << c;
+        if (c == 10)
+        {
+            line++;
+            std::cout << line << ": ";
+        }
+        c = f.get();
     }
-
 }
-
